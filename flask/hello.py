674ab6ac933235f1ba4fae3ee44c56243dc13b0b
Paul <paulvelthuis93@gmail.com>
2018-09-21 15:34:41 +0200

    redis and flask app please start with start.sh

--- flask/hello.py
+++ flask/hello.py
@@ -1,9 +1,30 @@
 from flask import Flask
+from flask import request, abort
+import redis
+import time
+import traceback
+
 app = Flask(__name__)
+# TODO fix exception
+r = redis.StrictRedis(host='redis.default.svc.cluster.local', port=6379)                          # Connect to local Redis instance
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    lines= """Hello World! <br> You can go to endpoint get to get the information, <br> or post to post the information """
+    return lines
+
+@app.route("/url", methods=["GET"])
+def get():
+    message = r.lpop('q')                                               # Checks for message
+    return message.decode("utf-8") + " get from redis!"
+
+@app.route("/url", methods=["POST"])
+def post():
+    if not request.json or not 'url' in request.json:
+        abort(400)
+    urlstring = request.json['url']
+    r.rpush("q", urlstring)
+    return "\'" + urlstring + "\' posted to redis!"
 
 def main():
     app.run(port=6666, host="0.0.0.0")
