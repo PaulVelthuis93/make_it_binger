d849cf82ca2eddb7582df74005af53429be9fa82
marilena <marilena.panagiotidou@gmail.com>
2018-09-21 15:48:07 +0200

    elasticsearch ready

--- app/main.py
+++ app/main.py
@@ -3,10 +3,11 @@ from bs4 import BeautifulSoup
 import json
 import logging
 import time
-
+from elasticsearch import Elasticsearch
 
 def get_url_from_redis():
-    # url = requests.get("http://redis-restapi.nl/get-url")
+    # try TODO: Catch connection problems
+    # url = requests.get("http://binger-api.default.svc.cluster.local/url")
     url = "https://kafka.apache.org/intro"
     return url
 
@@ -14,12 +15,20 @@ def get_body(da_link):
     r = requests.get(da_link)
     # encoding = r.encoding
     body = r.text
-    return body
+    title = BeautifulSoup(body).title.text
+    return {"body": body, "title": title}
 
-def send_body_to_elastic(body):
+def send_body_to_elastic(body, url, urls, title):
+
+    es_body = {"doc":{"body":body, "url": url, "links": list(urls), "title": title, "timestamp": time.time()}}
     print("******************** body_to_elastic ********************")
-    print(body)
-    pass
+    print(es_body)
+    # try:
+    #     # title, timestamp, links
+    #     es = Elasticsearch(["http://elasticsearch:9200/"])
+    #     es.update(body=es_body)
+    # except:
+    #     logging.warn("What am i doing with my life???????")
 
 def get_urls_from_body(body, url):
     soup = BeautifulSoup(body)
@@ -34,9 +43,9 @@ def get_urls_from_body(body, url):
 def send_urls_to_redis(links):
     # send to redis
     print("******************** urls_to_redis ********************")
-    print(links)
+    # print(links)
     try:
-        result = requests.post("http://redis-restapi.nl/dump-urls", data=json.dumps(list(links)))
+        result = requests.post("http://binger-api.default.svc.cluster.local/url", data=json.dumps(list(links)))
         if result.status_code != 200:
             logging.warn(f'Redis replied with a {result.status_code}')
     except:
@@ -45,9 +54,9 @@ def send_urls_to_redis(links):
 while True:
     url = get_url_from_redis()
     if url :
-        body = get_body(url)
-        send_body_to_elastic(body)
-        urls = get_urls_from_body(body, url)
+        res = get_body(url)
+        urls = get_urls_from_body(res['body'], url)
+        send_body_to_elastic(res['body'], url, urls, res['title'])
         send_urls_to_redis(urls)
     else:
         time.sleep(10)
